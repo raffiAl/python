c0a4649005069e22a63819facc0c053469c34eaf
raffiAl <silverfank01@gmail.com>
2025-03-01 17:15:53 +0700

    tugas sesi 3 | operator, I/O

---
+++ stateflow.py
@@ -0,0 +1,51 @@
+# Tugas 1: Kondisi kompleks dengan operator logika. Done!
+
+a = int(input("Masukan bilangan bulat: "))
+b = int(input("Masukan bilangan bulat: "))
+c = int(input("Masukan bilangan bulat: "))
+
+def kondisi_kompleks(a, b, c):
+  if (a > b or b > c):
+   if (a % 2 == 0 and c % 2 != 0):
+    if (b != c):
+      print("Kondisi terpenuhi")
+      return
+    
+  print("kondisi belum terpenuhi")
+      
+kondisi_kompleks(a, b, c)
+
+# Tugas 2: Pengecekan type dengan operasi logika. Done!
+
+def type_checker(string, integer, Ifloat):
+  if isinstance(string, str):
+    print("type input valid")
+  else:
+    print('type input invalid')
+
+  if isinstance(integer, int):
+    print("type input valid")
+  else:
+    print("type input invalid")
+
+  if isinstance(Ifloat, float):
+    print("type input valid")
+  else:
+    print("type input invalid")
+
+type_checker('kucing', 80, 3.4)
+
+# Tugas 3: Operator Logika dengan Nilai Boolean dan Konversi Tipe
+
+def im_afunction(x, y):
+  konversi_x = bool(x)
+  konversi_y = bool(y)
+ 
+  return(
+    konversi_x and konversi_y,
+    konversi_x or konversi_y,
+    not konversi_x,
+    x != y
+  )
+
+print(im_afunction(1, 1))
